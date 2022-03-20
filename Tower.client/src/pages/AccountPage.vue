022acc032123fef749c3814f6a36b8a67a27c493
TyHafen <tyhafen@gmail.com>
2022-03-19 22:22:59 -0600

    commit

--- Tower.client/src/pages/AccountPage.vue
+++ Tower.client/src/pages/AccountPage.vue
@@ -1,8 +1,28 @@
 <template>
-  <div class="about text-center">
-    <h1>Welcome {{ account.name }}</h1>
-    <img class="rounded" :src="account.picture" alt="" />
-    <p>{{ account.email }}</p>
+  <div class="about container-fluid">
+    <div class="row p-2 m-2">
+      <h3 class="text-light">My events</h3>
+    </div>
+    <div class="row">
+      <!-- iteration of my events! -->
+      <div class="col-md-2">
+        <div class="card bg-success shadow" style="width: ">
+          <img
+            class="card-img-top"
+            src="https://thiscatdoesnotexist.com/"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h6 class="card-text">
+              <div class="row p-1">Event Title</div>
+              <div class="row p-1">Location</div>
+              <div class="row p-1">date</div>
+              <div class="row p-1 justify-content-end">tickets left</div>
+            </h6>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,7 +40,4 @@ export default {
 </script>
 
 <style scoped>
-img {
-  max-width: 100px;
-}
 </style>
